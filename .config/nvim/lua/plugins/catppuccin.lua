8c6132a3523f41d5736c07b0a8f8b78f3544ee51
techolsy <alexander@techolsy.eu>
2025-02-18 16:41:56 +0100

    catppuccin.lua: fixed indentation and removed trailing whitespace

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,8 +1,8 @@
-return { 
-  "catppuccin/nvim", 
-  name = "catppuccin", 
-  priority = 1000, 
-	init = function()
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  init = function()
     vim.cmd.colorscheme "catppuccin"
-	end
+  end
 }
